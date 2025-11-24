39b7c41d72bf8c236e8c846b5ccf02025759b6a5
Stefan Springer <stefan.springer@icloud.com>
2025-11-24 17:53:51 +0100

    updated platforms

--- Package.swift
+++ Package.swift
@@ -1,10 +1,16 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "LoggingInterfaces",
+    platforms: [
+        .macOS(.v15),
+        .iOS(.v17),
+        .tvOS(.v17),
+        .watchOS(.v10),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
